6bdde895424ca508685ade0091b4e6d1355a1f62
DanieII <danielbogdanov1748@gmail.com>
2023-04-13 23:20:08 +0300

    Tried selenium for scraping the needed information

--- line_screen.py
+++ line_screen.py
@@ -6,34 +6,49 @@ from kivy.uix.scrollview import ScrollView
 from kivymd.uix.button import MDRaisedButton
 from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelOneLine
 import subway_info
+from datetime import datetime
 
 
 class Content(BoxLayout):
-    pass
+
+    def __init__(self, line, direction, station, current_hour, **kwargs):
+        super().__init__(**kwargs)
+        self.line = line
+        self.direction = direction
+        self.station = station
+        self.current_hour = current_hour
+        self.get_closest_arrivals()
+
+    def get_closest_arrivals(self):
+        schedule = subway_info.get_station_schedule(self.direction, self.station, self.line)
 
 
 class DirectionInformation(ModalView):
 
-    def __init__(self, line, direction):
+    def __init__(self, line, direction, current_hour):
         super().__init__()
         self.size_hint = (.85, .85)
+        self.current_hour = current_hour
+        self.stations: dict = subway_info.get_stations(line)
+        # self.stations = {'МЕТРОСТАНЦИЯ ГОРНА БАНЯ': '3336', 'МЕТРОСТАНЦИЯ ОВЧА КУПЕЛ II': '3334',
+        #                  'МЕТРОСТАНЦИЯ МИЗИЯ/НБУ': '3332', 'МЕТРОСТАНЦИЯ ОВЧА КУПЕЛ': '3330',
+        #                  'МЕТРОСТАНЦИЯ ЦАР БОРИС III / КРАСНО СЕЛО': '3328', 'МЕТРОСТАНЦИЯ БУЛ. БЪЛГАРИЯ': '3324',
+        #                  'МЕТРОСТАНЦИЯ МЕДИЦИНСКИ УНИВЕРСИТЕТ': '3322', 'МЕТРОСТАНЦИЯ НДК 2': '3320',
+        #                  'МЕТРОСТАНЦИЯ СВ. ПАТРИАРХ ЕВТИМИЙ': '3318', 'МЕТРОСТАНЦИЯ ОРЛОВ МОСТ': '3316',
+        #                  'МЕТРОСТАНЦИЯ ТЕАТРАЛНА': '3312', 'МЕТРОСТАНЦИЯ ХАДЖИ ДИМИТЪР': '3310'}
         self.line = line
         self.direction = direction
-        # self.stations: dict = subway_info.get_stations(line)
-        self.stations = {'МЕТРОСТАНЦИЯ ГОРНА БАНЯ': '3336', 'МЕТРОСТАНЦИЯ ОВЧА КУПЕЛ II': '3334',
-                         'МЕТРОСТАНЦИЯ МИЗИЯ/НБУ': '3332', 'МЕТРОСТАНЦИЯ ОВЧА КУПЕЛ': '3330',
-                         'МЕТРОСТАНЦИЯ ЦАР БОРИС III / КРАСНО СЕЛО': '3328', 'МЕТРОСТАНЦИЯ БУЛ. БЪЛГАРИЯ': '3324',
-                         'МЕТРОСТАНЦИЯ МЕДИЦИНСКИ УНИВЕРСИТЕТ': '3322', 'МЕТРОСТАНЦИЯ НДК 2': '3320',
-                         'МЕТРОСТАНЦИЯ СВ. ПАТРИАРХ ЕВТИМИЙ': '3318', 'МЕТРОСТАНЦИЯ ОРЛОВ МОСТ': '3316',
-                         'МЕТРОСТАНЦИЯ ТЕАТРАЛНА': '3312', 'МЕТРОСТАНЦИЯ ХАДЖИ ДИМИТЪР': '3310'}
         self.create_expansion_panels_for_each_station()
 
     def create_expansion_panels_for_each_station(self):
         stations_grid = GridLayout(cols=1, spacing=(0, 10), size_hint_y=None, size_hint_x=None, width=1000)
 
         for station in self.stations:
-            panel = MDExpansionPanel(panel_cls=MDExpansionPanelOneLine(text=station), icon="subway", content=Content())
+            station_id = self.stations[station]
+            panel = MDExpansionPanel(panel_cls=MDExpansionPanelOneLine(text=station), icon="subway",
+                                     content=Content(self.line, self.direction, station_id, self.current_hour))
             stations_grid.add_widget(panel)
+        subway_info.close_driver()
         stations_grid.bind(minimum_height=stations_grid.setter('height'))
 
         scroll_view = ScrollView(do_scroll_x=False)
@@ -49,12 +64,15 @@ class Line(Screen):
         self.line = line
 
     def on_enter(self):
-        # directions = subway_info.get_directions(self.line)
-        directions = {'м.Витоша-м.Обеля-м.Летище София': '2666', 'м.Летище София-м.Обеля-м.Витоша': '2667',
-                      'м.Витоша-м.Обеля-м.Бизнес Парк': '2668', 'м.Бизнес Парк-м.Обеля-м.Витоша': '2669'}
+        directions = subway_info.get_directions(self.line)
+        # directions = {'м.Витоша-м.Обеля-м.Летище София': '2666', 'м.Летище София-м.Обеля-м.Витоша': '2667',
+        #               'м.Витоша-м.Обеля-м.Бизнес Парк': '2668', 'м.Бизнес Парк-м.Обеля-м.Витоша': '2669'}
         for d in directions:
-            button = MDRaisedButton(text=d, font_style="Button", md_bg_color=[.8, .8, .8, 1], text_color=[.08, .07, .09, 1],
-                                    on_release=lambda x, direction=d: self.get_current_direction_modal_view(direction))
+            button = MDRaisedButton(text=d, font_style="Button", md_bg_color=[.8, .8, .8, 1],
+                                    text_color=[.08, .07, .09, 1],
+                                    padding=(20, 20),
+                                    on_release=lambda x, direction=d: self.get_current_direction_modal_view(direction,
+                                                                                                            directions))
             self.ids.directions.add_widget(button)
 
     def reset(self):
@@ -63,6 +81,6 @@ class Line(Screen):
     def get_departing_times(self):
         pass
 
-    def get_current_direction_modal_view(self, direction):
-        view = DirectionInformation(self.line, direction)
+    def get_current_direction_modal_view(self, direction, directions):
+        view = DirectionInformation(self.line, directions[direction], datetime.now().hour)
         view.open()
--- subway_info.py
+++ subway_info.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def get_line_soup(line):
@@ -35,10 +37,12 @@ def get_stations(line):
     return stations
 
 
-def get_station_schedule(direction, station, line):
+def test(direction, station, line):
     result = BeautifulSoup(
-        requests.get(f"https://schedules.sofiatraffic.bg/metro/{line}#sign/{direction}/{station}").content, "html.parser"
+        requests.get(f"https://schedules.sofiatraffic.bg/metro/{line}#sign/{direction}/{station}").content,
+        "html.parser"
     )
+    print(result.find("span", {"id": "schedule_10757_direction_4424_current_sign_name"}))
     times_information = result.find("div", {"class": "schedule_times"}).find("tbody").find_all("td")
     times = {}
 
@@ -46,3 +50,79 @@ def get_station_schedule(direction, station, line):
         times[time["data-cell"]] = time.text.split()
 
     return times
+
+
+def get_station_schedule(direction, station, line):
+    global driver
+    if not driver:
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(options=chrome_options)
+    url = f"https://schedules.sofiatraffic.bg/metro/{line}#sign/{direction}/{station}"
+    driver.get(url)
+    element = driver.find_element(By.CLASS_NAME, "schedule_times").find_element(By.TAG_NAME, "tbody")
+    div_element = driver.find_element(By.CLASS_NAME, "schedule_times")
+    tbody_element = div_element.find_elements(By.TAG_NAME, "td")
+    [print(x.text.strip()) for x in tbody_element]
+    print("\n" * 10)
+    driver.refresh()
+
+
+def close_driver():
+    global driver
+    driver.close()
+    driver = None
+
+
+class WebDriver:
+
+    def __init__(self):
+        self.driver: webdriver.Chrome = None
+        self.options = webdriver.ChromeOptions()
+        self.options.add_argument('--headless')
+        self.url = "https://schedules.sofiatraffic.bg/metro/"
+
+    @staticmethod
+    def merge_link(base, *args):
+        line, *rest = args
+        fragment = ""
+        if rest:
+            fragment = "#direction/" + "/".join(rest)
+        return base + line + fragment
+
+    @staticmethod
+    def get_soup(url):
+        html = requests.get(url)
+        soup = BeautifulSoup(html.content, "html.parser")
+        return soup
+
+    def get_directions(self, line):
+        soup = self.get_soup(self.merge_link(self.url, line))
+        directions_information = soup.find("ul", {"class": "schedule_view_direction_tabs"}).find_all("li")
+        directions = {}
+        link_pattern = r"(.*)\/([0-9]+)"
+
+        for information in directions_information:
+            current_direction = information.find("a")
+            direction_id = re.match(link_pattern, current_direction["href"]).group(2)
+            directions[current_direction.find("span").text] = direction_id
+
+        return directions
+
+    def get_stations(self, line, direction):
+        self.driver = webdriver.Chrome(options=self.options)
+        url = self.merge_link(self.url, line, direction)
+        self.driver.get(url)
+        stations_information = self.driver.find_element(By.CLASS_NAME, "schedule_direction_sign_wrapper").find_elements(
+            By.TAG_NAME, "li")
+        pattern = r"(.*)#([0-9]+)"
+        stations = {}
+
+        for station in stations_information:
+            station_id = re.match(pattern,
+                                  station.find_element(By.CLASS_NAME, "stop_link").get_attribute("href")).group(2)
+            station_name = station.find_element(By.CLASS_NAME, "stop_change").get_attribute("text")
+            stations[station_name] = station_id
+
+        self.driver.close()
+        return stations
